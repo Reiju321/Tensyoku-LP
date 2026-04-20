2bd875cd77378e307f47cc5440791a7f799678c6
Reiju321 <reijuueda0321@gmail.com>
2026-04-20 16:39:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(express.static('public'));
